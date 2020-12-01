a6a6bb0b3d0eccdb6dfc0c4cea0df63b6b9acd34
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-01 20:06:54 +0100

    Day 1

---
+++ dec_1/src/main.rs
@@ -0,0 +1,20 @@
+type CustomErr = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), CustomErr> {
+	let inputs = include_str!("input.txt")
+		.lines()
+		.map(|x| x.parse::<usize>().unwrap())
+		.collect::<Vec<usize>>();
+
+	for (index_1, &lhs) in inputs.iter().enumerate() {
+		for (index_2, &rhs) in inputs.iter().enumerate().skip(index_1) {
+			for ths in inputs.iter().skip(index_2) {
+				if lhs + rhs + ths == 2020 {
+					println!("{} * {} * {} = {}", lhs, rhs, ths, ths * lhs * rhs);
+				}
+			}
+		}
+	}
+
+	Ok(())
+}
