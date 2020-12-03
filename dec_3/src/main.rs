b04f5a902684866b19c6a6aefc1736716c307e17
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-03 11:48:23 +0100

    Day 3 refactor

--- dec_3/src/main.rs
+++ dec_3/src/main.rs
@@ -1,29 +1,23 @@
 fn main() {
 	let inputs = include_str!("input.txt").lines().collect::<Vec<_>>();
-	let sol1 = part_1(&inputs, 3);
+	let sol1 = part_1(&inputs, 3, 1);
 	println!("{}", sol1);
-	let sol2 = [
-		part_2(&inputs, 1, 1),
-		part_2(&inputs, 3, 1),
-		part_2(&inputs, 5, 1),
-		part_2(&inputs, 7, 1),
-		part_2(&inputs, 1, 2),
+	let sol2 = part_2(&inputs);
+	println!("{}", sol2);
+}
+
+fn part_2(inputs: &[&str]) -> usize {
+	let part_1_sols = [
+		part_1(&inputs, 1, 1),
+		part_1(&inputs, 3, 1),
+		part_1(&inputs, 5, 1),
+		part_1(&inputs, 7, 1),
+		part_1(&inputs, 1, 2),
 	];
-	println!("{:?}, {}", sol2, sol2.iter().product::<usize>());
+	part_1_sols.iter().product()
 }
 
-fn part_1(inputs: &[&str], r_slope: usize) -> usize {
-	inputs
-		.iter()
-		.enumerate()
-		.filter(|(x, line)| {
-			let line = line.as_bytes();
-			line[(x * r_slope) % line.len()] == b'#'
-		})
-		.count()
-}
-
-fn part_2(inputs: &[&str], r_slope: usize, d_slope: usize) -> usize {
+fn part_1(inputs: &[&str], r_slope: usize, d_slope: usize) -> usize {
 	inputs
 		.iter()
 		.enumerate()
