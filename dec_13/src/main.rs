8e08a68a50a9aacdc2e0bcd21f50726d8488af90
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-13 18:37:39 +0100

    Partial Day 13 part 2

--- dec_13/src/main.rs
+++ dec_13/src/main.rs
@@ -1,5 +1,3 @@
-use rayon::prelude::*;
-
 fn main() {
 	let input = include_str!("input.txt");
 	let sol1 = solve1(input);
@@ -9,7 +7,7 @@ fn main() {
 
 fn solve1(input: &str) -> usize {
 	let linebreak = input.chars().position(|c| c == '\n').unwrap();
-	let time_stamp = &input[..linebreak].parse().unwrap();
+	let time_stamp = input[..linebreak].parse::<usize>().unwrap();
 	let (bus, min_wait) = input[(linebreak + 1)..]
 		.split(',')
 		.filter_map(|s| s.parse::<usize>().ok())
@@ -19,40 +17,40 @@ fn solve1(input: &str) -> usize {
 	bus * min_wait
 }
 
-fn solve2(input: &str) -> isize {
+fn solve2(input: &str) -> usize {
 	let linebreak = input.chars().position(|c| c == '\n').unwrap();
 	let mut buses = input[(linebreak + 1)..]
 		.split(',')
 		.enumerate()
 		.map(|(i, s)| (i, s.parse()))
 		.filter(|(_, x)| x.is_ok())
-		.map(|(a, b)| (a as isize, b.unwrap()))
-		.collect::<Vec<(isize, isize)>>();
-	buses.sort_unstable_by_key(|(_, a)| *a);
+		.map(|(a, b)| (a, b.unwrap()))
+		.map(|(a, b)| ((16 * b - a) % b, b))
+		.collect::<Vec<(usize, usize)>>();
 	dbg!(&buses);
-	let first = (buses[0].1, buses[0].0);
-	let res = buses
+	buses.sort_unstable_by_key(|(_, a)| *a);
+	let (lcm, res) = buses
 		.iter()
 		.skip(1)
-		.fold(first, |(x, r), &(a, b)| find_mod(x, r, b, (b - a).abs()))
-		.1;
+		.fold((buses[0].1, buses[0].0), |(x, r), &(a, b)| {
+			find_mod(x, r, b, a)
+		});
 
+	assert_eq!(lcm, buses.iter().map(|(_, p)| p).product());
 	for &(index, prim) in buses.iter() {
 		assert_eq!(res % prim, index, "{} {}", index, prim);
 	}
 
-	res + buses[buses.len() - 1].0
+	res
 }
 
-fn find_mod(x1: isize, r1: isize, x2: isize, r2: isize) -> (isize, isize) {
-	assert!(x2 != 0);
+fn find_mod(x1: usize, r1: usize, x2: usize, r2: usize) -> (usize, usize) {
 	let lcm = x1 * x2;
-	assert!(lcm != 0);
 	let r2 = (r2 + x2 * 16) % x2;
 	let r1 = (r1 + x1 * 16) % x1;
-	dbg!((x1, r1, x2, r2));
-	(
-		lcm,
-		dbg!((r1..).step_by(x1 as usize).find(|&x| x % x2 == r2).unwrap()),
-	)
+	let res = (r1..=lcm)
+		.step_by(x1 as usize)
+		.find(|&x| x % x2 == r2)
+		.unwrap();
+	(lcm, res)
 }
