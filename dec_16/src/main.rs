bd20d6219c6e71db2d6cb625079aa59f3103de54
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-16 22:05:09 +0100

    Slightly better, I guess

--- dec_16/src/main.rs
+++ dec_16/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 fn main() {
 	let input = include_str!("input.txt").lines().collect::<Vec<_>>();
 	let sol1 = solve1(&input);
@@ -9,28 +11,8 @@ fn solve1(input: &[&str]) -> u64 {
 	let mut valid_ranges = input
 		.iter()
 		.take(20)
-		.flat_map(|l| {
-			let mut i = l.char_indices();
-			let mut flip = true;
-			let mut find_next = || {
-				flip = !flip;
-				i.find(|&(_, c)| flip ^ c.is_ascii_digit())
-					.map(|(i, _)| i)
-					.unwrap()
-			};
-			let start_of_n1 = find_next();
-			let end_of_n1 = find_next();
-			let start_of_n2 = find_next();
-			let end_of_n2 = find_next();
-			let start_of_n3 = find_next();
-			let end_of_n3 = find_next();
-			let start_of_n4 = find_next();
-			let n1 = l[start_of_n1..end_of_n1].parse::<u64>().unwrap();
-			let n2 = l[start_of_n2..end_of_n2].parse::<u64>().unwrap();
-			let n3 = l[start_of_n3..end_of_n3].parse::<u64>().unwrap();
-			let n4 = l[start_of_n4..].parse::<u64>().unwrap();
-			(n1..n2).chain(n3..n4)
-		})
+		.map(parse_limit_line)
+		.flat_map(|(a, b)| a.chain(b))
 		.collect::<Vec<_>>();
 	valid_ranges.sort_unstable();
 	valid_ranges.dedup();
@@ -46,22 +28,7 @@ fn solve2(input: &[&str]) -> u64 {
 	let valid_ranges = input
 		.iter()
 		.take(20)
-		.map(|l| {
-			let len = l.len();
-			let mut i = l.char_indices();
-			let mut flip = true;
-			let mut find_next = || {
-				flip = !flip;
-				i.find(|&(_, c)| flip ^ c.is_ascii_digit())
-					.map(|(i, _)| i)
-					.unwrap_or(len)
-			};
-			let mut next_section = || {
-				l[find_next()..find_next()].parse::<u64>().unwrap()
-					..l[find_next()..find_next()].parse::<u64>().unwrap()
-			};
-			(next_section(), next_section())
-		})
+		.map(parse_limit_line)
 		.collect::<Vec<_>>();
 	let my_ticket = parse_line(&input[22]);
 	let valid_tickets = input
@@ -69,55 +36,75 @@ fn solve2(input: &[&str]) -> u64 {
 		.skip(25)
 		.map(parse_line)
 		.filter(|v| {
-			v.iter().all(|&n| {
+			v.iter().all(|n| {
 				valid_ranges
 					.iter()
-					.any(|(r1, r2)| (n >= r1.start && n < r1.end) || (n >= r2.start && n < r2.end))
+					.any(|(r1, r2)| r1.contains(n) || r2.contains(n))
 			})
 		})
 		.collect::<Vec<_>>();
-	valid_tickets.iter().for_each(|v| assert!(v.len() == 20));
+	assert!(valid_tickets.iter().all(|v| v.len() == 20));
+
 	let mut possible = (0..20)
 		.map(|target| {
-			valid_tickets.iter().fold([true; 20], |mut acc, curr| {
-				acc.iter_mut().zip(curr.iter()).for_each(|(a, &t)| {
-					let (r1, r2) = &valid_ranges[target];
-					*a = *a && ((r1.start <= t && t <= r1.end) || (r2.start <= t && t <= r2.end))
+			let (r1, r2) = &valid_ranges[target];
+			let mut acc = [true; 20];
+			valid_tickets.iter().for_each(|curr| {
+				acc.iter_mut().zip(curr.iter()).for_each(|(a, c)| {
+					*a &= r1.contains(c) || r2.contains(c);
 				});
-				acc
-			})
+			});
+			acc
 		})
 		.collect::<Vec<_>>();
-	let mut i = 0;
-	loop {
-		i = (i + 1) % possible.len();
-		let sum = possible[i].iter().map(|&v| v as u8).sum::<u8>();
-		if sum == 1 {
-			let index = possible[i].iter().position(|&x| x).unwrap();
-			for (_, line) in possible.iter_mut().enumerate().filter(|&(j, _)| j != i) {
-				line[index] = false;
-			}
-			if possible
-				.iter()
-				.map(|l| l.iter().map(|&v| if v { 1 } else { 0 }).sum::<u8>())
-				.all(|x| x == 1)
-			{
-				break;
-			}
+
+	for line_idx in (0..possible.len()).cycle() {
+		if possible[line_idx].iter().map(|&v| v as u8).sum::<u8>() != 1 {
+			continue;
+		}
+		let column_idx = possible[line_idx].iter().position(|&x| x).unwrap();
+		for line in possible.iter_mut() {
+			line[column_idx] = false;
+		}
+		possible[line_idx][column_idx] = true;
+		if possible
+			.iter()
+			.map(|l| l.iter().map(|&v| v as u8).sum::<u8>())
+			.all(|x| x == 1)
+		{
+			break;
 		}
 	}
+
 	possible
 		.iter()
 		.take(6)
 		.map(|v| {
-			let i = v.iter().position(|&x| x).unwrap();
-			my_ticket[i]
+			let idx = v.iter().position(|&x| x).unwrap();
+			my_ticket[idx]
 		})
 		.product()
 }
 
-fn parse_line(l: &&str) -> Vec<u64> {
-	l.split(',')
+fn parse_line(line: &&str) -> Vec<u64> {
+	line.split(',')
 		.map(|n| n.parse::<u64>().unwrap())
 		.collect::<Vec<_>>()
+}
+
+fn parse_limit_line(line: &&str) -> (RangeInclusive<u64>, RangeInclusive<u64>) {
+	let len = line.len();
+	let mut idx = line.char_indices();
+	let mut flip = true;
+	let mut find_next = || {
+		flip = !flip;
+		idx.find(|&(_, c)| flip ^ c.is_ascii_digit())
+			.map(|(idx, _)| idx)
+			.unwrap_or(len)
+	};
+	let mut next_section = || {
+		line[find_next()..find_next()].parse::<u64>().unwrap()
+			..=line[find_next()..find_next()].parse::<u64>().unwrap()
+	};
+	(next_section(), next_section())
 }
