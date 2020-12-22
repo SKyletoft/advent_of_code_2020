2577792222d9df4f3ff55305eb680e6ca8ae4c0d
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-22 18:36:58 +0100

    Optimisation (and cut back of accuracy. Turns out only 30 bits were needed)

--- dec_22/src/main.rs
+++ dec_22/src/main.rs
@@ -1,14 +1,14 @@
-use std::collections::{BTreeSet, HashSet, VecDeque};
+use std::collections::{HashSet, VecDeque};
 
 fn main() {
 	let input = include_str!("input.txt").lines().collect::<Vec<_>>();
-	let sol1 = solve1(&input);
-	let sol2 = solve2(&input);
+	let (player1, player2) = parse_input(&input);
+	let sol1 = solve1(player1.clone(), player2.clone());
+	let sol2 = solve2(player1, player2);
 	println!("{} {}", sol1, sol2);
 }
 
-fn solve1(input: &[&str]) -> u64 {
-	let (mut player1, mut player2) = parse_input(input);
+fn solve1(mut player1: VecDeque<u8>, mut player2: VecDeque<u8>) -> u64 {
 	while !player1.is_empty() && !player2.is_empty() {
 		let p1 = player1.pop_front().unwrap();
 		let p2 = player2.pop_front().unwrap();
@@ -23,16 +23,12 @@ fn solve1(input: &[&str]) -> u64 {
 	calculate_score(&player1, &player2)
 }
 
-fn solve2(input: &[&str]) -> u64 {
-	let (mut player1, mut player2) = parse_input(input);
-	let mut history = BTreeSet::new();
-	let res = recursive_combat(&mut player1, &mut player2, &mut history);
+fn solve2(mut player1: VecDeque<u8>, mut player2: VecDeque<u8>) -> u64 {
+	let res = recursive_combat(&mut player1, &mut player2);
 	match res {
-		Some(true) | None => player2.clear(),
-		Some(false) => player1.clear(),
-	}
-	dbg!(&player1);
-	dbg!(&player2);
+		true => &mut player2.clear(),
+		false => &mut player1.clear(),
+	};
 	calculate_score(&player1, &player2)
 }
 
@@ -53,45 +49,36 @@ fn parse_input(input: &[&str]) -> (VecDeque<u8>, VecDeque<u8>) {
 }
 
 fn calculate_score(player1: &VecDeque<u8>, player2: &VecDeque<u8>) -> u64 {
-	player1
+	let p1 = player1
 		.iter()
 		.rev()
 		.enumerate()
 		.map(|(i, &v)| (i as u64 + 1) * v as u64)
-		.sum::<u64>()
-		+ player2
-			.iter()
-			.rev()
-			.enumerate()
-			.map(|(i, &v)| (i as u64 + 1) * v as u64)
-			.sum::<u64>()
+		.sum::<u64>();
+	let p2 = player2
+		.iter()
+		.rev()
+		.enumerate()
+		.map(|(i, &v)| (i as u64 + 1) * v as u64)
+		.sum::<u64>();
+	p1.max(p2)
 }
 
-fn recursive_combat(
-	player1: &mut VecDeque<u8>,
-	player2: &mut VecDeque<u8>,
-	history: &mut BTreeSet<GameState>,
-) -> Option<bool> {
+fn recursive_combat(player1: &mut VecDeque<u8>, player2: &mut VecDeque<u8>) -> bool {
+	let mut history = HashSet::new();
 	while !player1.is_empty() && !player2.is_empty() {
-		let history_entry = GameState::from_iter(
-			player1
-				.iter()
-				.chain([0u8].iter())
-				.chain(player2.iter())
-				.copied(),
-		);
+		let history_entry = GameState::from_iter(player1.iter().copied());
 		if history.contains(&history_entry) {
-			return None;
+			return true;
 		} else {
 			history.insert(history_entry);
 		}
 		let p1 = player1.pop_front().unwrap();
 		let p2 = player2.pop_front().unwrap();
 		let winner = if player1.len() as u8 >= p1 && player2.len() as u8 >= p2 {
-			let mut c1 = player1.clone();
-			let mut c2 = player2.clone();
-			let mut h = BTreeSet::new();
-			recursive_combat(&mut c1, &mut c2, &mut h)?
+			let mut c1 = player1.iter().take(p1 as usize).copied().collect();
+			let mut c2 = player2.iter().take(p2 as usize).copied().collect();
+			recursive_combat(&mut c1, &mut c2)
 		} else {
 			p1 > p2
 		};
@@ -103,28 +90,20 @@ fn recursive_combat(
 			player2.push_back(p1);
 		}
 	}
-	Some(player2.is_empty())
+	player2.is_empty()
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct GameState {
-	a: u128,
-	b: u128,
-	c: u128,
+	a: u32, //The more bits, the lower chance of collisions.
+	        //6 bits per number up to a total of 6 * 51 for 100% accuracy
 }
 
 impl GameState {
 	fn from_iter(i: impl Iterator<Item = u8> + Clone) -> Self {
-		let iter = i.map(|n| n as u128);
-		let mut gs = GameState { a: 0, b: 0, c: 0 };
-		for n in iter.clone().take(21) {
-			gs.a = (gs.a << 6) + n;
-		}
-		for n in iter.clone().skip(21 * 2).take(21) {
-			gs.b = (gs.b << 6) + n;
-		}
-		for n in iter.skip(21 * 3).take(21) {
-			gs.c = (gs.c << 6) + n;
+		let mut gs = GameState { a: 0 };
+		for n in i.map(|n| n as u32).take(32 / 6) {
+			gs.a = (gs.a << 6) | n;
 		}
 		gs
 	}
