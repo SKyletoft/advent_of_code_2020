566d2b3f6412cbc293eaf5b8d9a7e08fc8b229f3
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-24 11:24:39 +0100

    Dec 24 added, not done

---
+++ dec_24/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+	let input = include_str!("example.txt").lines().collect::<Vec<_>>();
+	let sol1 = solve1(&input);
+	let sol2 = solve2(&input);
+	println!("{} {}", sol1, sol2);
+}
+
+fn solve1(input: &[&str]) -> u64 {
+	0
+}
+
+fn solve2(input: &[&str]) -> u64 {
+	0
+}
