6abe5e26c25d1daf53aad88577307c855ae3d7c7
Samuel Kyletoft <skyletoft@gmail.com>
2020-12-26 23:41:00 +0100

    style fix and less stupidity

--- dec_25/src/main.rs
+++ dec_25/src/main.rs
@@ -13,17 +13,18 @@ fn solve1(input: &[u64]) -> u64 {
 	let door_public_key = input[0];
 	let card_public_key = input[1];
 
-	//let door_loop_size = solve_transform(7, door_public_key);
+	let door_loop_size = solve_transform(7, door_public_key);
 	let card_loop_size = solve_transform(7, card_public_key);
 
 	let encryption_key_1 = transform(door_public_key, card_loop_size);
-	//let encryption_key_2 = transform(card_public_key, door_loop_size);
-	//assert_eq!(encryption_key_1, encryption_key_2);
+	let encryption_key_2 = transform(card_public_key, door_loop_size);
+
+	assert_eq!(encryption_key_1, encryption_key_2);
 	encryption_key_1
 }
 
 fn transform(sub_num: u64, loops: u64) -> u64 {
-	pow2(sub_num, loops as u32, MOD)
+	pow_mod(sub_num, loops as u32, MOD)
 }
 
 //Based on u64::pow in the standard library
@@ -45,11 +46,5 @@ const fn pow_mod(mut base: u64, mut exp: u32, m: u64) -> u64 {
 }
 
 fn solve_transform(sub_num: u64, target: u64) -> u64 {
-	for x in 0.. {
-		let res = transform(sub_num, x);
-		if res == target {
-			return x;
-		}
-	}
-	unreachable!();
+	(0..).find(|&l| transform(sub_num, l) == target).unwrap()
 }
